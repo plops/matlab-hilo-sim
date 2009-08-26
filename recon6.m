847692f09e77b6a9314da212da4897872792efdd
mr chickenfarmer <martin@torben.(none)>
2009-08-26 16:50:30 +0100

    ilp was complex that lead to artifacts when computing ihilo

--- recon6.m
+++ recon6.m
@@ -13,7 +13,8 @@ ins=dbl2(gratings,0);
 wf=1;
 iu=dbl2(wf,0);
 %% illuminate only in-focus parts of the object
-slice_perfect=gaussf(squeeze(obj(:,:,floor(s3/2))>.1));
+%gaussf
+slice_perfect=bdilation(squeeze(obj(:,:,floor(s3/2))>.1));
 iu2=dbl2(slice_perfect,0);
 %% illuminate in-focus parts of the object with a grating
 in2=dbl2(slice_perfect & grating,0);
@@ -24,14 +25,14 @@ ins2=dbl2(slice_perfect & gratings,0);
 skpsf=squeeze(sum(kpsf,[],3));
 corr=gaussf((rr(skpsf,'freq')<.42),3)./skpsf; % use this to correct for otf
 %%
-ft(2.5*in2-iu2).*corr
+ft(2.06612*in2-iu2).*corr
 %% shift right order into the center
 %diff=ins2-in2; add=ins2+in2;
 %diff=ins-iu; add=iu;
-diff=2.5*in2-iu2; add=iu2;
+diff=1.55231*in2-iu2; add=iu2;
 o=ft(ift((ft(diff).*corr)).*exp(-i*2*pi*(xx(in,'freq').*32)));
 %%
-kc=0.07203;
+kc=0.04;
 r=rr(in,'freq');
 klp=exp(-r.^2/(2*kc^2)); % low pass filter in k-space
 % low resolution slice
@@ -40,7 +41,7 @@ ilp=ift(o.*klp)
 ihp=ift(ft(add).*corr.*(1-klp));
 
 %%
-ihilo=ihp+3.63637*ilp
+ihilo=ihp+20*ilp
 %ihilo(64,:)
 
 %%
