6be26b854767820a018d4c488d576c7a58e5c90e
mr chickenfarmer <martin@torben.(none)>
2009-08-27 12:28:37 +0100

    did the simulation for laser illumination. seems to work fine as well (at least for this sample)

--- recon6.m
+++ recon6.m
@@ -1,39 +1,40 @@
 % simulate clem
 %% has to be run as initialization
 run './dbl.m'
+laser=1;
 %% image with grating
 period=4;
 grating=mod(rx,period)>((period/2)-1);
-in=dbl2(grating,0);
+in=dbl2(grating,laser);
 %% image with shifted grating
 gratings=mod(rx+period/2,period)>((period/2)-1);
-ins=dbl2(gratings,0);
+ins=dbl2(gratings,laser);
 
 %% widefield image
 wf=1;
-iu=dbl2(wf,0);
+iu=dbl2(wf,laser);
 %% illuminate only in-focus parts of the object
 %gaussf
 slice_perfect=bdilation(squeeze(obj(:,:,floor(s3/2))>.1),1);
-iu2=dbl2(slice_perfect,0);
+iu2=dbl2(slice_perfect,laser);
 %% illuminate in-focus parts of the object with a grating
-in2=dbl2(slice_perfect & grating,0);
+in2=dbl2(slice_perfect & grating,laser);
 %% illuminate in-focus parts of the object with the shifted grating
-ins2=dbl2(slice_perfect & gratings,0);
+ins2=dbl2(slice_perfect & gratings,laser);
 
 %% for the clem image remove in-focus rectangle in right top
 rectangle=newim(slice_perfect)>1;
 rectangle(83:end,23:43)=1;
 slice_clem=slice_perfect & ~ bdilation(rectangle);
-iu3=dbl2(slice_clem,0);
+iu3=dbl2(slice_clem,laser);
 %%
-in3=dbl2(slice_clem & grating,0);
+in3=dbl2(slice_clem & grating,laser);
 
 %% only illuminate the bright rectangle
 not_slice_clem=bdilation(rectangle);
-iu4=dbl2(not_slice_clem,0);
+iu4=dbl2(not_slice_clem,laser);
 %%
-in4=dbl2(not_slice_clem & grating,0);
+in4=dbl2(not_slice_clem & grating,laser);
 
 %% project otf along z
 skpsf=squeeze(sum(kpsf,[],3));
